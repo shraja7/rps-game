20650cfa5f23ef2b871dd348fa331997f3b9f6b7
shraja7 <shraja7@gmail.com>
2022-05-23 22:41:15 -0500

    implement one round logic

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 const computerChoice = ['rock', 'paper', 'scissors']
 //seelcts random computer choice
 const computerPlay = (computerChoice) => { 
-
+return computerChoice[Math.floor(Math.random()*computerChoice.length)]
  }
+
+//get player selection
+const playerSelection = 'rock';
+
+ //function that plays a single round of the game
+ const playRound = (playerSelection, computerSelection) => { 
+
+   //case of draw
+   if(playerSelection === computerSelection){
+       return 'draw'
+   }  
+     //cases where player wins
+if(playerSelection  === 'rock' && computerSelection === 'scissors'){
+    return 'player wins'
+}else if(playerSelection === 'paper' && computerSelection === 'rock'){
+    return 'player wins'
+}else if(playerSelection === 'scissors' && computerSelection ==='paper'){
+    return 'player wins'
+}
+    //cases where computer wins
+else if(computerSelection === 'rock' && playerSelection === 'scissors'){
+    return 'computer wins'
+}else if(computerSelection === 'paper' && playerSelection === 'rock'){
+    return 'computer wins'
+}else if(computerSelection === 'scissors' && playerSelection === 'paper'){
+    return 'computer wins'
+}
+
+   
+  }
+
+  
+
+  
